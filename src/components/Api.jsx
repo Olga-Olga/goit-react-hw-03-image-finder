d5730c59459303740b84220af0214d3cb06ad117
Olga-Olga <olga_l@ukr.net>
2023-08-06 00:06:23 +0300

    search

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -6,10 +6,11 @@ const URL =
   'https://pixabay.com/api/?q=cat&page=1&key=38199190-357b16d82ad5caa1ff3a80ee2&image_type=photo&orientation=horizontal&per_page=12';
 
 export const fetchImages = async params => {
+  console.log(params);
   const { data } = await axios.get(axios.defaults.baseURL, {
     params: {
       key: API_KEY,
-      q: '',
+      q: 'tree',
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,26 @@
 import { ImageGallery } from './ImageGallery';
 import { Searchbar } from './Searchbar';
 import React, { Component } from 'react';
-import axios from 'axios';
 import { fetchImages } from './Api';
 
 // axios.defaults.headers.common['Authorization'] = API_KEY;
 
 export class App extends Component {
   state = {
-    searchWord: 'tree',
+    searchWord: 'world',
     hits: [],
     total: '',
     totalHits: '',
+    per_page: 4,
   };
 
   async componentDidMount() {
     // if (prevState.searchWord !== this.state.searchWord) {
+
+    const { searchWord: q, per_page } = this.state;
+    console.log(q, per_page);
     try {
-      const res = await fetchImages();
+      const res = await fetchImages({ q, per_page });
       console.log('res:');
       console.dir(res);
       this.setState({
@@ -32,6 +35,7 @@ export class App extends Component {
 
   handleSearchInput = word => {
     this.setState({ searchWord: word });
+    console.log(this.state);
   };
 
   render() {
