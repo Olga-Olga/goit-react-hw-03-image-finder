f5bca237a3c93256da1208e4b5b3c8e0aca51241
Olga-Olga <olga_l@ukr.net>
2023-08-06 20:00:01 +0300

    fix not used files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import { ImageGallery } from './ImageGallery';
 import { Searchbar } from './Searchbar';
 import React, { Component } from 'react';
 import { fetchImages } from './Api';
-import { ImageGalleryItem } from './ImageGalleryItem';
 import { Button } from './Button';
 import { Circles } from 'react-loader-spinner';
 import { Modal } from './Modal';
